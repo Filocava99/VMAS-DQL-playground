6f178a368d03eaa367d611254090fecb40d0ddfd
Filippo Cavallari <filippo.cavallari99@gmail.com>
2023-08-24 17:38:58 +0200

    Add cohesion collision

--- Cleaning.py
+++ Cleaning.py
@@ -1,3 +1,4 @@
+import math
 import random
 import typing
 from typing import Dict, Callable, List
@@ -19,6 +20,7 @@ if typing.TYPE_CHECKING:
 class Scenario(BaseScenario):
     def make_world(self, batch_dim: int, device: torch.device, **kwargs) -> World:
         self.n_agents = kwargs.get("n_agents", 5)
+        self.target_distance = 20.0
         self.n_targets = kwargs.get("n_targets", 7)
         self._min_dist_between_entities = kwargs.get("min_dist_between_entities", 0.2)
         self._lidar_range = kwargs.get("lidar_range", 0.35)
@@ -53,9 +55,6 @@ class Scenario(BaseScenario):
         entity_filter_agents: Callable[[Entity], bool] = lambda e: e.name.startswith(
             "agent"
         )
-        entity_filter_targets: Callable[[Entity], bool] = lambda e: e.name.startswith(
-            "target"
-        )
         for i in range(self.n_agents):
             # Constraint: all agents have same action range and multiplier
             agent = Agent(
@@ -63,189 +62,62 @@ class Scenario(BaseScenario):
                 collide=True,
                 shape=Sphere(radius=self.agent_radius),
                 sensors=[
-                    # Lidar(
-                    #     world,
-                    #     angle_start=0.05,
-                    #     angle_end=2 * torch.pi + 0.05,
-                    #     n_rays=12,
-                    #     max_range=self._lidar_range,
-                    #     entity_filter=entity_filter_agents,
-                    #     render_color=Color.BLUE,
-                    # ),
                     Lidar(
                         world,
                         n_rays=15,
                         max_range=self._lidar_range,
-                        entity_filter=entity_filter_targets,
-                        render_color=Color.GREEN,
+                        entity_filter=entity_filter_agents,
+                        render_color=Color.BLUE,
                     ),
                 ],
             )
-            agent.collision_rew = torch.zeros(batch_dim, device=device)
-            agent.covering_reward = agent.collision_rew.clone()
             world.add_agent(agent)
-
-        self._targets = []
-        for i in range(self.n_targets):
-            target = Landmark(
-                name=f"target_{i}",
-                collide=True,
-                movable=False,
-                shape=Sphere(radius=self.target_radius),
-                color=self.target_color,
-            )
-            world.add_landmark(target)
-            self._targets.append(target)
-
-        self.covered_targets = torch.zeros(batch_dim, self.n_targets, device=device)
-        self.shared_covering_rew = torch.zeros(batch_dim, device=device)
 
         return world
 
     def reset_world_at(self, env_index: int = None):
-        placable_entities = self._targets[: self.n_targets] + self.world.agents
-        if env_index is None:
-            self.all_time_covered_targets = torch.full(
-                (self.world.batch_dim, self.n_targets), False, device=self.world.device
-            )
-        else:
-            self.all_time_covered_targets[env_index] = False
         ScenarioUtils.spawn_entities_randomly(
-            entities=placable_entities,
+            entities=self.world.agents,
             world=self.world,
             env_index=env_index,
             min_dist_between_entities=self._min_dist_between_entities,
             x_bounds=(-self.world.x_semidim, self.world.x_semidim),
             y_bounds=(-self.world.y_semidim, self.world.y_semidim),
         )
-        for target in self._targets[self.n_targets:]:
-            target.set_pos(self.get_outside_pos(env_index), batch_index=env_index)
 
-    def reward(self, agent: Agent):
-        targets = self._targets
-        targets_positions = torch.stack([t.state.pos for t in targets], dim=1)
-        diffs = targets_positions - agent.state.pos.unsqueeze(1)
-        # Calculate Euclidean distance (L2 norm)
-        distances = torch.norm(diffs, dim=-1)
-        # Get the minimum distance and its index
-        min_distance, min_index = torch.min(distances, dim=-1)
-        covered_targets = (min_distance < self._covering_range).type(torch.int)
-        if self.world.batch_dim > 1:
-            min_distance -= min_distance.min(-1, keepdim=True)[0]
-            min_distance /= min_distance.max(-1, keepdim=True)[0]
-        min_distance = 1 - min_distance
-        min_distance[covered_targets != 0] = 100
-        self.respawn_targets(agent)
+
+
+    def cohesionFactor(self, distances):
+        max_distance = distances.max(dim=1, keepdim=True).values
+        mask = (max_distance <= self.target_distance)
+        max_distance[mask] = 0.0
+        mask = (max_distance > self.target_distance)
+        max_distance[mask] -= self.target_distance
+        max_distance[mask] *= -1
+        return max_distance
+
+    def collisionFactor(self, distances):
+        min_distance = distances.min(dim=1, keepdim=True).values
+        mask = (min_distance > self.target_distance)
+        min_distance[mask] = 0.0
+        mask = (min_distance <= self.target_distance)
+        min_distance[mask] /= self.target_distance
+        min_distance[mask].log_()
+        min_distance[mask] *= 2
         return min_distance
 
-    def respawn_targets(self, agent: Agent):
-        targets = self._targets
-        targets_positions = torch.stack([t.state.pos for t in targets], dim=0)
-        diffs = targets_positions - agent.state.pos.unsqueeze(0)
+    def reward(self, agent: Agent):
+        agents = self.world.agents
+        agents_without_agent = list(filter(lambda x: x != agent, agents))
+        agents_positions = torch.stack([t.state.pos for t in agents_without_agent], dim=1)
+        diffs = agents_positions - agent.state.pos.unsqueeze(1)
         # Calculate Euclidean distance (L2 norm)
         distances = torch.norm(diffs, dim=-1)
-        in_range = distances < self._covering_range
-        for(i, target) in enumerate(targets):
-            target.state.pos[in_range[i]] = self.random_pos()[in_range[i]]
-
-    def random_pos(self):
-        x_coord = torch.full((self.world.batch_dim, 1), random.uniform(-self.world.x_semidim, self.world.x_semidim))
-        y_coord = torch.full((self.world.batch_dim, 1), random.uniform(-self.world.y_semidim, self.world.y_semidim))
-        tensor = torch.cat((x_coord, y_coord), dim=1)
-        return tensor
-
-    def first_reward(self, agent: Agent):
-        is_first = agent == self.world.agents[0]
-        is_last = agent == self.world.agents[-1]
-
-        if is_first:
-            self.time_rew = torch.full(
-                (self.world.batch_dim,), self.time_penalty, device=self.world.device
-            )
-            self.agents_pos = torch.stack(
-                [a.state.pos for a in self.world.agents], dim=1
-            )
-            self.targets_pos = torch.stack([t.state.pos for t in self._targets], dim=1)
-            self.agents_targets_dists = torch.cdist(self.agents_pos, self.targets_pos)
-            self.agents_per_target = torch.sum(
-                (self.agents_targets_dists < self._covering_range).type(torch.int),
-                dim=1,
-            )
-            self.covered_targets = self.agents_per_target >= self._agents_per_target
-
-            self.shared_covering_rew[:] = 0
-            for a in self.world.agents:
-                self.shared_covering_rew += self.agent_reward(a)
-            self.shared_covering_rew[self.shared_covering_rew != 0] /= 2
-
-        # Avoid collisions with each other
-        agent.collision_rew[:] = 0
-        for a in self.world.agents:
-            if a != agent:
-                agent.collision_rew[
-                    self.world.get_distance(a, agent) < self.min_collision_distance
-                    ] += self.agent_collision_penalty
-
-        if is_last:
-            if self.targets_respawn:
-                occupied_positions_agents = [self.agents_pos]
-                for i, target in enumerate(self._targets):
-                    occupied_positions_targets = [
-                        o.state.pos.unsqueeze(1)
-                        for o in self._targets
-                        if o is not target
-                    ]
-                    occupied_positions = torch.cat(
-                        occupied_positions_agents + occupied_positions_targets, dim=1
-                    )
-                    pos = ScenarioUtils.find_random_pos_for_entity(
-                        occupied_positions,
-                        env_index=None,
-                        world=self.world,
-                        min_dist_between_entities=self._min_dist_between_entities,
-                        x_bounds=(-self.world.x_semidim, self.world.x_semidim),
-                        y_bounds=(-self.world.y_semidim, self.world.y_semidim),
-                    )
-
-                    target.state.pos[self.covered_targets[:, i]] = pos[
-                        self.covered_targets[:, i]
-                    ].squeeze(1)
-            else:
-                self.all_time_covered_targets += self.covered_targets
-                for i, target in enumerate(self._targets):
-                    target.state.pos[self.covered_targets[:, i]] = self.get_outside_pos(
-                        None
-                    )[self.covered_targets[:, i]]
-        covering_rew = (
-            agent.covering_reward
-            if not self.shared_reward
-            else self.shared_covering_rew
-        )
-
-        return agent.collision_rew + covering_rew + self.time_rew
-
-    def get_outside_pos(self, env_index):
-        return torch.empty(
-            (1, self.world.dim_p)
-            if env_index is not None
-            else (self.world.batch_dim, self.world.dim_p),
-            device=self.world.device,
-        ).uniform_(-1000 * self.world.x_semidim, -10 * self.world.x_semidim)
-
-    def agent_reward(self, agent):
-        agent_index = self.world.agents.index(agent)
-
-        agent.covering_reward[:] = 0
-        targets_covered_by_agent = (
-                self.agents_targets_dists[:, agent_index] < self._covering_range
-        )
-        num_covered_targets_covered_by_agent = (
-                targets_covered_by_agent * self.covered_targets
-        ).sum(dim=-1)
-        agent.covering_reward += (
-                num_covered_targets_covered_by_agent * self.covering_rew_coeff
-        )
-        return agent.covering_reward
+        # Calculate cohesion factor
+        cohesion_factor = self.cohesionFactor(distances)
+        # Calculate collision factor
+        collision_factor = self.collisionFactor(distances)
+        return cohesion_factor + collision_factor
 
     def observation(self, agent: Agent):
         lidar_1_measures = agent.sensors[0].measure()
@@ -263,29 +135,17 @@ class Scenario(BaseScenario):
 
     def info(self, agent: Agent) -> Dict[str, Tensor]:
         info = {
-            "covering_reward": agent.covering_reward
-            if not self.shared_reward
-            else self.shared_covering_rew,
-            "collision_rew": agent.collision_rew,
-            "targets_covered": self.covered_targets.sum(-1),
         }
         return info
 
     def done(self):
-        return self.all_time_covered_targets.all(dim=-1)
+        return torch.zeros(self.world.batch_dim, dtype=torch.bool, device=self.world.device)
 
     def extra_render(self, env_index: int = 0) -> "List[Geom]":
         from vmas.simulator import rendering
 
         geoms: List[Geom] = []
         # Target ranges
-        for i, target in enumerate(self._targets):
-            range_circle = rendering.make_circle(self._covering_range, filled=False)
-            xform = rendering.Transform()
-            xform.set_translation(*target.state.pos[env_index])
-            range_circle.add_attr(xform)
-            range_circle.set_color(*self.target_color.value)
-            geoms.append(range_circle)
         # Communication lines
         for i, agent1 in enumerate(self.world.agents):
             for j, agent2 in enumerate(self.world.agents):
